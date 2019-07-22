b238dd78d204607715f479e6119901eee1ba5a87
Marcin Polonis <marcin.polonis@o2.pl>
2019-07-22 19:50:35 +0200

    Change syntax and add styles for navbar and footer components

--- my-shop/src/components/Footer/Footer.js
+++ my-shop/src/components/Footer/Footer.js
@@ -4,20 +4,15 @@ import './Footer.scss';
 
 export const Footer = props => {
     return (
-        <div>
-            <nav className='bottomNavbar'>
-                <NavLink exact to='/home' activeClassName='active'>
-                    Home
-                </NavLink>
-                <NavLink exact to='/FAQ' activeClassName='active'>
-                    FAQ
-                </NavLink>
-                <NavLink exact to='/regulamin' activeClassName='active'>
-                    Regulamin
-                </NavLink>
-                <NavLink exact to='/kontakt' activeClassName='active'>
-                    Kontakt
-                </NavLink>
+        <div>  
+            <nav className="footer">
+                <ul className="nav-bottom">
+                    <li><h3 className="rights">All Rights Reserved</h3></li>
+                    <li><NavLink exact to="/home" className="active" activeClassName="active">Home</NavLink></li>
+                    <li><NavLink exact to="/FAQ" activeClassName="active">FAQ</NavLink></li>
+                    <li><NavLink exact to="/regulamin" activeClassName="active">Regulamin</NavLink></li>
+                    <li><NavLink exact to="/kontakt" activeClassName="active">Kontakt</NavLink></li>
+                </ul>
             </nav>
         </div>
     )
